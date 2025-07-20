1f2dc27a477187627d3047bec3ebb62d54292a28
Thomas Reynoso Cordero <thomas.reynoso@externos.w2m.com>
2025-07-20 02:14:03 +0200

    fix npm run type-check & build

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,7 +7,7 @@ export function debounce<T extends (...args: never[]) => void>(
   func: T,
   wait: number
 ): (...args: Parameters<T>) => void {
-  let timeout: number
+  let timeout: ReturnType<typeof setTimeout>
   return (...args: Parameters<T>) => {
     clearTimeout(timeout)
     timeout = setTimeout(() => func(...args), wait)
